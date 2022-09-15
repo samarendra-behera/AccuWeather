cb3a9524c7c3386b5f6bc5550243c1ac066adea0
samarendra behera <71862123+samarendra-behera@users.noreply.github.com>
2022-09-15 21:20:48 +0530

    script modify for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs');
 
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 const staticPath = path.join(__dirname, "../public");
 const partialsPath = path.join(__dirname, '../templates/partials');
 const viewsPath = path.join(__dirname, '../templates/views')
